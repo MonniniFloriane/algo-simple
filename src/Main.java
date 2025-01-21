a4f04ff9f6640e2a2ae000060748b2d57423cace
dev_flo <florianemonnini@gmail.com>
2025-01-21 14:42:31 +0100

    three commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     //FONCTION
-    public static Integer SomeIT(int[] tblImpaire) {
+    // /////////EXO1
+    public static Integer someIT(int[] tblImpaire) {
         int total  = 0;
         //BOUCLE
         for (int i = 0; i < tblImpaire.length; i++) {
@@ -11,13 +12,35 @@ public class Main {
         return total;
     }
 
+    public static String returnMotPlusLong(String[] tblMotPlusLong) {
+        String motPlusLong = "";
+
+        //BOUCLE
+        for (int i = 0; i < tblMotPlusLong.length; i++) {
+            if (tblMotPlusLong[i].length() > motPlusLong.length()) {
+                motPlusLong = tblMotPlusLong[i];
+            }
+        }
+        return motPlusLong;
+    }
+
     public static void main(String[] args) {
+        // /////////EXO1
         int[] tab1 = {5,7,12,5};
         int[] tab2 = {8,3,5,6};
         //AFFICHAGE
-        int totalTab1 = SomeIT(tab1);
-        int totalTab2 = SomeIT(tab2);
+        int totalTab1 = someIT(tab1);
+        int totalTab2 = someIT(tab2);
 
+        System.out.println("EXO 1 : Nombre paire et impaire + somme");
         System.out.println(totalTab1 + totalTab2);
+
+        // /////////EXO2
+        String[] stringTab = {"Hello", "World", "fldsjnsdfdnsfn" ,"Testtest"};
+        //AFFICHAGE
+        String tabMotPlusLong = returnMotPlusLong(stringTab);
+
+        System.out.println("EXO 2 : Return mot le plus long");
+        System.out.println(tabMotPlusLong);
     }
 }
